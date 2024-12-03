cd8c1c26e62785f1d1a90e25357fd50e261cdd5c
HAKKIM6747 <Kim081674792!@#>
2024-12-03 12:00:12 +0700

    Styling table

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [],
+  imports: [MatPaginatorModule],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
